6b01daee54dac8963c77cd0243095c46bfaeb1f0
Naoyuki Kumagai <kumagai@namaka>
2023-09-05 14:00:46 +0900

    Modify Artist to has_many :albums

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,9 @@
 class Artist < ApplicationRecord
   self.primary_key = :artist_id
 
+  has_many :albums_artists
+  has_many :albums, through: :albums_artists
+
   MAJOR_COLUMNS = %i[artist_id name mb_name]
 
   scope :select_major, -> { select(MAJOR_COLUMNS) }
